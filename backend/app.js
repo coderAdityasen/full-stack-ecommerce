17017defd4fa38ac7546ec3d7f76f9b2500a7d40
coderAdityasen <adityasen561@gmail.com>
2024-05-16 14:29:12 +0530

    middleware change

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,9 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
-import productRouter from "./routes/product.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
-import openairoutes from "./routes/openai.routes.js";
-import orderRoute from "./routes/Order.routes.js";
+
 
 const app = express()
-
-
 
 app.use(
 	cors({
@@ -19,17 +13,21 @@ app.use(
   );
   
   
-app.use(cookieParser());
-
-app.use(express.json())
-
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
-app.use(express.static("public"));
+  app.use(express.json({limit: "16kb"}))
+  app.use(express.urlencoded({extended: true, limit: "16kb"}))
+  app.use(express.static("public"))
+  app.use(cookieParser())
 
 
 
 //routes
+
+import productRouter from "./routes/product.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import openairoutes from "./routes/openai.routes.js";
+import orderRoute from "./routes/Order.routes.js";
+
 
 app.use("/api" , productRouter)
 app.use("/user" , userRoutes)
