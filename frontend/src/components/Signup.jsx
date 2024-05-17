523997eb6f1529b67cfbb3bdc849d987d1c1f7fa
coderAdityasen <adityasen561@gmail.com>
2024-05-17 07:56:54 +0530

    signup preloader installed

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -5,26 +5,32 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { login } from '../reducers/userReducer'
 import axios from 'axios'; 
+import GridLoader from 'react-spinners/GridLoader'
 
 function Signup() {
 
 	const navigate = useNavigate()
 	const dispatch = useDispatch()
+  const [loading , setloading] = useState(false)
 	const [error , setError] = useState("")
 	const {register, handleSubmit} = useForm()
 
 	const submitbutton = async (data)=>{
 		try {
+      setloading(true)
 			const response =await axios.post("https://full-stack-ecommerce-api-jade.vercel.app/user/signup" , data)
 			try {
         dispatch(login({ userData: response.data.data }));
         dispatch(fetchcart(response.data.data._id));
+        setloading(false)
 				navigate("/")
 			} catch (error) {
+        setloading(false)
         reset()
 				console.log("error in dispatch" , error);
 			}
 		} catch (error) {
+      setloading(false)
 			setError("user already exist" , error)
 		}
 	}
@@ -32,6 +38,10 @@ function Signup() {
   return (
 	<>
 	<Navbar/>
+  {
+    loading ? ( <div className="flex w-full h-[100vh] m-auto items-center justify-center">
+    <GridLoader color="#36d7b7" />
+  </div>) : (
 	<section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
    
@@ -168,6 +178,9 @@ function Signup() {
     </div>
   </div>
 </section>
+  )
+  }
+
 
 	</>
   )
